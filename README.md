6f6aedd96d4a8f85d4a40086d7397d000b2973db
Matt Lehrer <lehrerm@gmail.com>
2020-04-10 14:15:35 -0700

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sapper TailwindCSS Starter
+
+This pulls together Sapper, Svelte 3, Tailwind CSS, and PurgeCSS to create a simple starter kit for the sveltest apps ever.
+
+## Development
+
+To clone it and get started:
+
+```bash
+git clone https://github.com/EricPKerr/sapper-tailwindcss.git
+cd sapper-tailwindcss
+npm install # or yarn!
+
+touch .env
+
+npm run dev
+```
+
+Open up [localhost:3000](http://localhost:3000).
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  // import { stores } from '@sapper/app';
+
+  // const { preloading } = stores();
+
+  // console.log('Preloading', preloading);
+
+  import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 1em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
