a19cf4714b68f14f1d258d3d8f59bd16b61c39bd
Matt Lehrer <lehrerm@gmail.com>
2020-05-13 17:19:33 -0700

    fix gray typo from boilerplate

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,59 +1,61 @@
-<div class="container mt-8 p-8 bg-gray-200">
+<div class="w-full bg-gray-200">
+  <footer class="container mt-8 p-8">
     <div class="sm:flex mb-4">
-  <div class="sm:w-1/4 h-auto">
-      <div class="text-orange-500 mb-2">Orange</div>
-      <ul class="list-reset leading-normal">
-          <li class="hover:text-orange-500 text-grey-700">One</li>
-          <li class="hover:text-orange-500 text-grey-700">Two</li>
-          <li class="hover:text-orange-500 text-grey-700">Three</li>
-          <li class="hover:text-orange-500 text-grey-700">Four</li>
-          <li class="hover:text-orange-500 text-grey-700">Five</li>
-          <li class="hover:text-orange-500 text-grey-700">Six</li>
-          <li class="hover:text-orange-500 text-grey-700">Seven</li>
-          <li class="hover:text-orange-500 text-grey-700">Eight</li>
-      </ul>
-  </div>
-  <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
-      <div class="text-blue-600 mb-2">Blue</div>
-      <ul class="list-reset leading-normal">
-          <li class="hover:text-blue-600 text-grey-700">One</li>
-          <li class="hover:text-blue-600 text-grey-700">Two</li>
-          <li class="hover:text-blue-600 text-grey-700">Three</li>
-      </ul>
-
-    <div class="text-blue-400 mb-2 mt-4">Light Blue</div>
-      <ul class="list-reset leading-normal">
-          <li class="hover:text-blue-400 text-grey-700">One</li>
-          <li class="hover:text-blue-400 text-grey-700">Two</li>
-          <li class="hover:text-blue-400 text-grey-700">Three</li>
-      </ul>
-
-  </div>
-  <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
-            <div class="text-green-500 mb-2">Green</div>
-      <ul class="list-reset leading-normal">
-          <li class="hover:text-green-500 text-grey-700">One</li>
-          <li class="hover:text-green-500 text-grey-700">Two</li>
-          <li class="hover:text-green-500 text-grey-700">Three</li>
-      </ul>
-
-    <div class="text-green-400 mb-2 mt-4">Light Green</div>
-      <ul class="list-reset leading-normal">
-          <li class="hover:text-green-400 text-grey-700">One</li>
-          <li class="hover:text-green-400 text-grey-700">Two</li>
-          <li class="hover:text-green-400 text-grey-700">Three</li>
-      </ul>
-
-
-  </div>
-    <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
-        <div class="text-red-400 mb-2">Newsletter</div>
-        <p class="text-grey-700 leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consectetur. </p>
-        <div class="mt-4 flex">
-            <input type="text" class="p-2 border border-grey-400 round text-grey-500 text-sm h-auto" placeholder="Your email address">
-            <button class="bg-orange-500 text-white rounded-sm h-auto text-xs p-3">Subscribe</button>
-        </div>
+      <div class="sm:w-1/4 h-auto">
+        <div class="text-orange-500 mb-2">Orange</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-orange-500 text-gray-700">One</li>
+          <li class="hover:text-orange-500 text-gray-700">Two</li>
+          <li class="hover:text-orange-500 text-gray-700">Three</li>
+          <li class="hover:text-orange-500 text-gray-700">Four</li>
+          <li class="hover:text-orange-500 text-gray-700">Five</li>
+          <li class="hover:text-orange-500 text-gray-700">Six</li>
+          <li class="hover:text-orange-500 text-gray-700">Seven</li>
+          <li class="hover:text-orange-500 text-gray-700">Eight</li>
+        </ul>
     </div>
-
-</div>
+    <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+        <div class="text-blue-600 mb-2">Blue</div>
+        <ul class="list-reset leading-normal">
+            <li class="hover:text-blue-600 text-gray-700">One</li>
+            <li class="hover:text-blue-600 text-gray-700">Two</li>
+            <li class="hover:text-blue-600 text-gray-700">Three</li>
+        </ul>
+  
+      <div class="text-blue-400 mb-2 mt-4">Light Blue</div>
+        <ul class="list-reset leading-normal">
+            <li class="hover:text-blue-400 text-gray-700">One</li>
+            <li class="hover:text-blue-400 text-gray-700">Two</li>
+            <li class="hover:text-blue-400 text-gray-700">Three</li>
+        </ul>
+  
+    </div>
+    <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+              <div class="text-green-500 mb-2">Green</div>
+        <ul class="list-reset leading-normal">
+            <li class="hover:text-green-500 text-gray-700">One</li>
+            <li class="hover:text-green-500 text-gray-700">Two</li>
+            <li class="hover:text-green-500 text-gray-700">Three</li>
+        </ul>
+  
+      <div class="text-green-400 mb-2 mt-4">Light Green</div>
+        <ul class="list-reset leading-normal">
+            <li class="hover:text-green-400 text-gray-700">One</li>
+            <li class="hover:text-green-400 text-gray-700">Two</li>
+            <li class="hover:text-green-400 text-gray-700">Three</li>
+        </ul>
+  
+  
+    </div>
+      <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+          <div class="text-red-400 mb-2">Newsletter</div>
+          <p class="text-gray-700 leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consectetur. </p>
+          <div class="mt-4 flex">
+              <input type="text" class="p-2 border border-gray-400 round text-gray-500 text-sm h-auto" placeholder="Your email address">
+              <button class="bg-orange-500 text-white rounded-sm h-auto text-xs p-3">Subscribe</button>
+          </div>
+      </div>
+  
+    </div>
+  </footer>
 </div>
