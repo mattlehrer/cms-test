4d8a7c969e621fcb301b578d4eff010ac1afa58d
Matt Lehrer <lehrerm@gmail.com>
2020-05-13 17:22:07 -0700

    setup css for wp posts

--- src/routes/blog/_PostSummary.svelte
+++ src/routes/blog/_PostSummary.svelte
@@ -1,11 +1,14 @@
 <script>
   export let post;
-  const { title, date, author, excerpt, slug } = post;
+  const { title, date, author, excerpt, slug, featured_image } = post;
 </script>
 
-<div class="mt-3 mb-5">
-  <div class="mb-2 text-xl text-gray-200"><a rel='prefetch' href="/blog/{slug}">{title}</a></div>
-  <div class="text-md text-gray-500">{@html excerpt}...</div>
+<div class="mt-3 mb-12">
+  {#if featured_image}
+    <img src={featured_image} alt='Featured Image for {title}' class="max-w-xs pb-2 pt-5" />
+  {/if}
+  <div class="text-xl text-gray-200"><a rel='prefetch' href="/blog/{slug}">{title}</a></div>
+  <div class="text-md text-gray-500 cms-content">{@html excerpt}</div>
   <div class="my-1 text-lg text-gray-200"><a rel='prefetch' href="/blog/{slug}">Continue Reading -></a></div>
   <div class="text-md text-gray-600">
     <span>{author.first_name ? author.first_name + ' ' + author.last_name : author.name}</span>
